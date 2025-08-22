1e7ce9e2e149d3ef3f2a8dc3a6c9431cbd4eb3a6
AbdulAhad87522 <ahadbahi263@gmail.com>
2025-08-22 10:00:41 +0500

    ..

---
+++ OOP/staticprop.js
@@ -0,0 +1,12 @@
+class user {
+    constructor(username,password){
+    this.username = username
+    this.password = password
+    }
+    static LogMe(){
+        console.log(`12`)
+    }
+}
+
+// let chai = new user("ahad" , "123")
+user.LogMe();
